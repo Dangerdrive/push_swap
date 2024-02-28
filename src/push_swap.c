0a8d029083679b5432536768f9b24a287134d69e
Dangerdrive <felippealencar@gmail.com>
2024-02-28 15:18:24 -0300

    - Commit on 2024.02.28\t15h18m23

--- src/push_swap.c
+++ src/push_swap.c
@@ -40,6 +40,78 @@ void	init_stack(t_stack_node stack)
 	stack->next = NULL;
 	stack->prev = NULL;
 }
+/*movements*/
+
+void	rr(t_stack_node **a, t_stack_node **b, bool print)
+{
+	rotate(a);
+	rotate(b);
+	if (!print)
+		ft_printf("rr\n");
+}
+
+void	rotate_both(t_stack_node **a, t_stack_node **b,
+	t_stack_node cheapest_node)
+{
+	while (*b != cheapest_node->target_node && *a != cheapest_node)
+		rr(a, b, false)
+	current_index(*a);
+	current_index(*b);
+}
+
+void	prep_for_push(t_stack_node **stack, t_stack_node *top_node,
+	char stack_name)
+{
+	while (*stack != top_node)
+	{
+		if (stack_name == 'a')
+		{
+			if (top_node->above_median)
+				ra(stack, false);
+			else
+				rra(stack, false);
+		}
+		else if (stack_name == 'b')
+		{
+			if (top_node->above_median)
+				rb(stack, false);
+			else
+				rrb(stack, false)
+		}
+	}
+}
+
+void	move_a_to_b(t_stack_node **a, t_stack_node **b)
+{
+	t_stack_node	*cheapest_node;
+
+	cheapest_node = get_cheapest(*a);
+	if (cheapest_node->above_median && cheapest_node->target_node->above_median)
+		rotate_both(a, b, cheapest_node);
+	else if (!(cheapest_node->above_median)
+		&& !(cheapest_node->target_node->above_median))
+		rev_rotate_both(a, b, cheapest_node);
+	prep_for_push(a, cheapest_node, 'a');
+	prep_for_push(b, cheapest_node->target_node, 'b');
+	pb(b, a, false);
+}
+
+void	move_b_to_a(t_stack_node **a, t_stack_node **b)
+{
+	prep_for_push(a, (*b)->target_node, 'a');
+	pa(a, b, false);
+}
+
+void	min_on_top(t_stack_node **a)
+{
+	while ((*a)->value != find_min(*a)->value)
+	{
+		if (find_min(*a)->above_median)
+			ra(a, false);
+		else
+			rra(a, false);
+	}
+}
 
 void	current_index(t_stack_node *stack)
 {
@@ -86,8 +158,8 @@ void	set_target_a(t_stack_node *a, t_stack_node *b)
 
 void	cost_analysis_a(t_stack_node *a, t_stack_node *b)
 {
-	size_t len_a;
-	size_t len_b;
+	size_t	len_a;
+	size_t	len_b;
 
 	len_a = stack_len(a);
 	len_b = stack_len(b);
@@ -99,7 +171,7 @@ void	cost_analysis_a(t_stack_node *a, t_stack_node *b)
 		if (a->target_node->above_median)
 			a->push_price += a->target_node->final_index;
 		else
-			a->push_price +=len_b - (a->target_node->final_index);
+			a->push_price += len_b - (a->target_node->final_index);
 		a = a->next;
 	}
 }
